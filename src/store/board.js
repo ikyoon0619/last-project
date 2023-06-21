6c4dd858acae4430a724df76ab995551064988cc
allegory <ikyoon0619@gmail.com>
2023-06-21 15:08:02 +0900

    board store method 추가
    id로 article 를 단건 요청하는 method

--- src/store/board.js
+++ src/store/board.js
@@ -54,15 +54,31 @@ export default {
         commit("updateState",{loading: false, isInit: false});
       }
     },
-    
+    async reqArticleWithId({state, commit}, payload){
+      try {
+        const res = await _fetchArticles({
+          ...payload,
+        });
+
+        commit("updateState",{ 
+          theArticle: res.data,
+        });
+        
+      } catch (error) {
+        commit("updateState", {
+          articles:[],
+        });
+      } 
+    },
   }
-  
 };
 
 function _fetchArticles(payload) {
-  const { searchType, text, size, page } = payload;
+  const { searchType, text, size, page, id } = payload;
 
-  const url = `http://localhost:8080/api/v1/articles?size=${size}&page=${page}`;
+  const url = id 
+  ? `http://localhost:8080/api/v1/article/${id}` 
+  : `http://localhost:8080/api/v1/articles?size=${size}&page=${page}`;
 
   return new Promise((resolve, reject) => {
     axios
