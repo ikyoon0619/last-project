b319352ecf8348d83e752d2644e834d2e2ce6246
allegory <ikyoon0619@gmail.com>
2023-06-21 15:15:02 +0900

    Artilcle 상세 화면 data fetch
    기사 클릭시 상세화면 컴포넌트에서 데이터 fetch하여 화면에 보여주는 로직 추가

--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -1,13 +1,31 @@
 
-<script setup>
-import {ref} from "vue";
+<script >
+import { mapState } from "vuex";
+import store from "../store"
+import Loader from "../components/Loader.vue";
+
+export default {
+  components:{
+    Loader,
+  },
+  computed:{
+    ...mapState("board",["theArticle"]),
+  },
+  created(){
+    store.dispatch("board/reqArticleWithId",{
+      id: this.$route.params.id
+    })
+  },
+}
+
 
 </script>
 <template>
-    <div class="article">
-      <h1>This is Article Detail View</h1>
-      
-    </div>
+  <!-- <Loader v-if="loading" /> -->
+  <div v-if="theArticle" class="article">
+    <h2>{{ theArticle.title }}</h2>
+    <p>{{ theArticle.content }}</p>
+  </div>
 </template>
   
 <style>
