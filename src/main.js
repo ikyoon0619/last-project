7812b402de3dbda0e71f89ee5961b60e04f777d2
allegory <ikyoon0619@gmail.com>
2023-06-22 11:12:34 +0900

    Dateformat 패키지 추가
    상세화면에 적용, 추후 게시글 리스트에도 반영 예정

--- src/main.js
+++ src/main.js
@@ -3,12 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import loadImage from "./plugins/loadImage";
+import DateFormat from '@voidsolutions/vue-dateformat'
 
 import "./assets/main.css";
 import "./scss/main.scss";
 
 const app = createApp(App);
 
-app.use(router).use(loadImage).use(store);
+app.use(router).use(loadImage).use(store).use(DateFormat);
 
 app.mount("#app");
