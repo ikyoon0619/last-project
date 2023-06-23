f045a31f0bfd842b9b206857f1416aeecc11d34a
allegory <ikyoon0619@gmail.com>
2023-06-23 16:20:31 +0900

    테스트를 위한 패키지 설치
    vitest 를 사용하기로 결정하고 설치함

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
@@ -10,5 +11,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  test:{
+    globals: true,
+    environment:'happy-dom'
   }
 })
