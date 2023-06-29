af157dab7c45c5c70736280608875c32f0684c10
allegory <ikyoon0619@gmail.com>
2023-06-29 12:43:12 +0900

    없는 컴포넌트 선언된 부분 정리

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <script setup>
 import HeadLine from "../components/HeadLine.vue";
-// import PharmacyList from "../components/PharmacyList.vue";
 import Search from "../components/Search.vue";
 import MovieList from "../components/movie/MovieList.vue"
 </script>
